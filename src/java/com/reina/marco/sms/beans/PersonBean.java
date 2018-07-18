f388b18cbf6053583a185486dc62991628eea8e3
marcoreina <marco.lima.reina@gmail.com>
2018-07-17 23:14:44 -0300

    Edit and remove person implemented.

--- src/java/com/reina/marco/sms/beans/PersonBean.java
+++ src/java/com/reina/marco/sms/beans/PersonBean.java
@@ -4,29 +4,49 @@ import com.reina.marco.sms.domain.Person;
 import com.reina.marco.sms.service.PersonService;
 import java.util.List;
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-import javax.inject.Named;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import org.primefaces.event.RowEditEvent;
 
-@Named
-@javax.enterprise.context.RequestScoped
+@ManagedBean
+@RequestScoped
 public class PersonBean {
     
-    @Inject
+    @EJB
     private PersonService personService;
     
-    List<Person> persons;
+    private Person selectedPerson;
+    
+    private List<Person> persons;
     
     public PersonBean(){}
 
     @PostConstruct
     public void initialize(){
         persons = personService.listPersons();
+        selectedPerson = new Person();
+    }
+    
+    public void addPerson(){
+        personService.registerPerson(this.selectedPerson);
+        persons = personService.listPersons();
+        this.selectedPerson = null;
+    }
+    
+    public void restartSelectedPerson(){
+        this.selectedPerson = new Person();
     }
     
     public void editListener(RowEditEvent event){
         Person person = (Person)event.getObject();
         personService.modifyPerson(person);
+    }
+    
+    public void deletePerson(){
+        personService.deletePerson(this.selectedPerson);
+        persons = personService.listPersons();
+        this.selectedPerson = null;
     }
     
     public List<Person> getPersons() {
@@ -35,5 +55,13 @@ public class PersonBean {
 
     public void setPersons(List<Person> persons) {
         this.persons = persons;
+    }
+
+    public Person getSelectedPerson() {
+        return selectedPerson;
+    }
+
+    public void setSelectedPerson(Person selectedPerson) {
+        this.selectedPerson = selectedPerson;
     }
 }

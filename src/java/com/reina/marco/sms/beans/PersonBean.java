7d1cb4e3ac314fdd7974ae072b71f8b5e2e39872
marcoreina <marco.lima.reina@gmail.com>
2018-07-17 21:18:32 -0300

    Add student feature implemented.

--- src/java/com/reina/marco/sms/beans/PersonBean.java
+++ src/java/com/reina/marco/sms/beans/PersonBean.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.reina.marco.sms.beans;
 
-/**
- *
- * @author marcoreina
- */
+import com.reina.marco.sms.domain.Person;
+import com.reina.marco.sms.service.PersonService;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.primefaces.event.RowEditEvent;
+
+@Named
+@javax.enterprise.context.RequestScoped
 public class PersonBean {
     
+    @Inject
+    private PersonService personService;
+    
+    List<Person> persons;
+    
+    public PersonBean(){}
+
+    @PostConstruct
+    public void initialize(){
+        persons = personService.listPersons();
+    }
+    
+    public void editListener(RowEditEvent event){
+        Person person = (Person)event.getObject();
+        personService.modifyPerson(person);
+    }
+    
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
 }
